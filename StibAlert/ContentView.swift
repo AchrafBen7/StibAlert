3dbcf60eb735e2bec33f1f15edd813f55b54161d
studentehb <studentehb@dt-imac-07.local>
2025-03-06 13:05:58 +0100

    Initial commit

---
+++ StibAlert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StibAlert
+//
+//  Created by studentehb on 06/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Lets go!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StibAlert/StibAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  StibAlertApp.swift
+//  StibAlert
+//
+//  Created by studentehb on 06/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct StibAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
